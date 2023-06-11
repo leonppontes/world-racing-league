d12a18cef0066a459848c9a920859f0bbc1fded6
unknown <leon.pontes@laceti.cin.ufpe.br>
2023-06-11 13:23:37 -0300

    wip

---
+++ testRaceRoute.ts
@@ -0,0 +1,6 @@
+import { Request, Response } from 'express';
+import { Pool } from 'pg';
+
+export async function testRaceHandler(req: Request, res: Response) {
+  // Route handler logic for creating drivers
+}
