def6899c456a2062a4966731731ec699e04561c2
unknown <leon.pontes@laceti.cin.ufpe.br>
2023-06-11 13:19:32 -0300

    gitignore

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { createDriversHandler } from './createDriversRoute';
 import { createTeamsHandler } from './createTeamsRoute';
+import { testRaceHandler } from './testRaceRoute';
 
 const app = express();
 const port = 3000;
@@ -8,6 +9,7 @@ const port = 3000;
 // Register the route handlers
 app.get('/createDrivers', createDriversHandler);
 app.get('/createTeams', createTeamsHandler);
+app.get('/testRace', testRaceHandler);
 
 // Start the server
 app.listen(port, () => {
